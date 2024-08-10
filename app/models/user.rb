30bbdc7b9554ce24378eec918b3913650b1d2334
daichi1997 <tagaaan@gmail.com>
2024-08-10 18:09:04 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :user_name,       presence: true
+  validates :user_profile,    presence: true
+  validates :user_occupation, presence: true
+  validates :user_position,   presence: true
+  validates :nickname,   presence: true
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_10_084816) do
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "user_name", null: false
+    t.text "user_profile", null: false
+    t.text "user_occupation", null: false
+    t.text "user_position", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
