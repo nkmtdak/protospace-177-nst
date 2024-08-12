673b3a1f25c54751dab4b9a7b3db094e9bb000f6
daichi1997 <tagaaan@gmail.com>
2024-08-12 16:52:31 +0900

    不具合修正

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_10_093243) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
